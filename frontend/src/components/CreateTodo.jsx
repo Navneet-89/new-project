3da7ae592d28a284b74619e0f9477fe141d0e171
Navneet <navneet_230089@aitpune.edu.in>
2024-08-17 19:15:32 +0530

    problem solved

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,11 +1,9 @@
 export function CreateTodo(){
-    retrun (
-        <div>
-            <input type="text" placeholder="title"></input>
-            <br/>
-            <input type="text" placeholder="description"></input> <br/>
-
-            <button>Add a todo</button>
-        </div>
-    )
-}
+    return <div>
+        <input type="text" placeholder="title"></input>
+        <br/>
+        <input type="text" placeholder="title"></input>
+        <br/>
+        <button>Add a Todo</button>
+    </div>
+};
